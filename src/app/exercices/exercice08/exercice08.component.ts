a4b359d2e16da344852d44f389db844f1f606368
QuentinGeerts <quentin.geerts@bstorm.be>
2023-08-04 12:32:51 +0200

    Exercice 08 - Générateur de formes

--- src/app/exercices/exercice08/exercice08.component.ts
+++ src/app/exercices/exercice08/exercice08.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercice08',
   templateUrl: './exercice08.component.html',
   styleUrls: ['./exercice08.component.scss']
 })
-export class Exercice08Component {
+export class Exercice08Component implements OnInit {
+
+  min: number = 50;
+  max: number = 250;
+
+  height: number = 50;
+  width: number = 50;
+  color: string = '#ffffff';
+  isCircle: boolean = false;
+
+  borderWidth: number = 1;
+  borderColor: string = '#000000';
+  borderStyle: string = 'solid';
+
+  styles: any;
+
+  ngOnInit (): void {
+    this.update();
+  }
+
+  update (): void {
+    this.styles = {
+      'height': this.height + 'px',
+      'width': this.width + 'px',
+      'background-color': this.color,
+      'border-width': this.borderWidth + 'px',
+      'border-color': this.borderColor,
+      'border-style': this.borderStyle,
+    };
+  }
 
 }
