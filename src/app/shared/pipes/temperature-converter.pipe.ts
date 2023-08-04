17435a81fdcd5097d35831c2525b1e54f5100bdc
QuentinGeerts <quentin.geerts@bstorm.be>
2023-08-04 10:51:24 +0200

    Fix Temperature converter

--- src/app/shared/pipes/temperature-converter.pipe.ts
+++ src/app/shared/pipes/temperature-converter.pipe.ts
@@ -7,32 +7,21 @@ export class TemperatureConverterPipe implements PipeTransform {
 
   transform (temperature: number, baseUnite: string, targetUnite: string): number {
 
-    if (baseUnite === 'fahrenheit') {
-      if (targetUnite === 'celsius') temperature = this.toCelsius(temperature);
-      else if (targetUnite === 'kelvin') temperature = this.toKelvin(temperature);
+    if (baseUnite === 'fahrenheit' && targetUnite === 'celsius') { temperature = this.toCelsius(temperature);
     }
-    else if (baseUnite === 'celsius') {
-      if (targetUnite === 'fahrenheit') temperature = this.toFahrenheit(temperature);
-      else if (targetUnite === 'kelvin') temperature = this.toKelvin(temperature);
-    }
-    else if (baseUnite === 'kelvin') {
-      if (targetUnite === 'celsius') temperature = this.toCelsius(temperature);
-      else if (targetUnite === 'fahrenheit') temperature = this.toFahrenheit(temperature);
+    else if (baseUnite === 'celsius' && targetUnite === 'fahrenheit') {
+      temperature = this.toFahrenheit(temperature);
     }
 
     return +(temperature).toFixed(3);
   }
 
   private toCelsius (temperature: number): number {
-    return (temperature * 9 / 5) + 32;
-  }
-
-  private toFahrenheit (temperature: number): number {
     return (temperature - 32) * 5 / 9;
   }
-
-  private toKelvin (temperature: number): number {
-    return (temperature - 273.15);
+  
+  private toFahrenheit (temperature: number): number {
+    return (temperature * 9 / 5) + 32;
   }
 
 }
